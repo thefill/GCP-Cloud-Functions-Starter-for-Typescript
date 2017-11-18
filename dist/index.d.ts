70aea21f8ecb84169572bcf7d5b2387e09236629
Filip Dabrowski <dabrowskif@uk.innovation-group.com>
2017-11-18 17:36:40 +0000

    Remove obsolete artifact, change tsconfig

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="express" />
+import { Request, Response } from "express";
+declare function helloWorldFunction(req: Request, res: Response): void;
+export { helloWorldFunction };
--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-/**
- * Cloud function definition
- */
-function helloWorldFunction(req, res) {
-    res.send("Hello " + (req.body.name || 'World') + "!");
-}
-exports.helloWorldFunction = helloWorldFunction;
